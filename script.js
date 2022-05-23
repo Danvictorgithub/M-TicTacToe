b36202d27d9cccadf2c29f9c4c2e6ca64ab05bf4
VictorWSL <danvictorprogramming1@gmail.com>
2022-05-23 18:57:34 +0800

    Added Scoping in script - Project Discontinued till exam

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
+
+//Game ON Logic
+const gameBoard = function() {
+}();
+
 // Player Factory Function
 const Player = (name,marker) => {
     return {name, marker};
 };
 // DOM Logic in each Grid
-(function() {
+function currentPlayer(marker) {
     const gridRow = document.querySelectorAll(".gridRow");
     gridRow.forEach((row) => {
         const childNodes = row.childNodes;
         childNodes.forEach((child) => {
-            child.addEventListener('click', () => {
-                child.textContent = 'X';
-            })
+            child.addEventListener('click', (e) => {
+                addMark(e,marker);
+            });
         });
     });
-})();
-    
+}
+currentPlayer('X');
+function addMark(e,marker) {
+    e.target.textContent = `${marker}`;
+}
+
+// Returns Array of the Game
+function returnGameBoardArray() {
+    const boardArray = [];
+    const _board = document.querySelectorAll('.board'); 
+    _board.forEach((board) => {
+        boardArray.splice(0,0,board.textContent)
+    });
+    return boardArray;
+}
